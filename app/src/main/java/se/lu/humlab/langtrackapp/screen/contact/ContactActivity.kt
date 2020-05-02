f67d76dfd91b5560e1c01ab498da1780c31f006a
stephanbjorck <stephan.bjorck@humlab.lu.se>
2020-05-02 12:11:00 +0200

    layout instructions, contact

--- app/src/main/java/se/lu/humlab/langtrackapp/screen/contact/ContactActivity.kt
+++ app/src/main/java/se/lu/humlab/langtrackapp/screen/contact/ContactActivity.kt
@@ -4,19 +4,27 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import kotlinx.android.synthetic.main.contact_activity.*
 import se.lu.humlab.langtrackapp.R
+import se.lu.humlab.langtrackapp.databinding.ContactActivityBinding
 
 class ContactActivity : AppCompatActivity() {
 
+
+    private lateinit var mBind :ContactActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.contact_activity)
+        mBind = DataBindingUtil.setContentView(this, R.layout.contact_activity)
+        mBind.lifecycleOwner = this
+        mBind.executePendingBindings()
 
-        contactCloseButton.setOnClickListener {
-            onBackPressed()
+        mBind.contactScroll.setOnScrollChangeListener { _, _, _, _, _ ->
+            mBind.contactTopView.isSelected = mBind.contactScroll.canScrollVertically(-1)
         }
     }
+
 
     companion object{
         fun start(context: Context){
